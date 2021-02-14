4103361077502fcf0c7b4fadaa793bac8c64dec7
b3aaa <rodrlehqk@naver.com>
2021-02-14 23:17:09 +0900

    71일차

---
+++ [71]chap07-2/src/sec02/test/Five.java
@@ -0,0 +1,8 @@
+package sec02.test;
+
+public class Five {
+	/*
+	 * 멤버 로그인
+	 * A 로그인
+	 */
+}
---
+++ [71]chap07-2/src/sec02/test/Two.java
@@ -0,0 +1,8 @@
+package sec02.test;
+
+public class Two {
+	/*
+	 * 스노우 타이어가 굴러갑니다.
+	 * 스노우 타이어가 굴러갑니다.
+	 */
+}
