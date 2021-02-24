1390b75f17f525e2941aa8830698132b4076efb5
b3aaa <rodrlehqk@naver.com>
2021-02-24 19:52:24 +0900

    79일차

---
+++ [79]chap13-2/src/sec02/exam02/Message.java
@@ -0,0 +1,11 @@
+package sec02.exam02;
+
+public class Message {
+	public String commend;
+	public String to;
+	
+	public Message(String command,String to) {
+		this.commend=command;
+		this.to=to;
+	}
+}
